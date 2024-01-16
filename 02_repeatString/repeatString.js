5a44f19a9c03fd1484f96ae75ce6227731388dec
ale-morillas <alecorm.edx@gmail.com>
2024-01-16 11:20:14 -0500

    Finish 02_repeatString exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, n) {
+  let string = "";
 
+  if (n < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < n; i++) {
+      string += str;
+    }
+
+    return string;
+  }
 };
 
 // Do not edit below this line
