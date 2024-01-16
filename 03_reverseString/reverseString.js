32b9cea8ede536a0a15f4795743ccb2d355e4848
ale-morillas <alecorm.edx@gmail.com>
2024-01-16 11:34:34 -0500

    Finish 03_reverseString exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  const stringArray = str.split("");
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += stringArray[i];
+  }
 
+  return reversedStr;
 };
 
 // Do not edit below this line
