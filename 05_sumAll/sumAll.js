4b3d7536cdce045927002627bca97e0f1dabdf09
ale-morillas <alecorm.edx@gmail.com>
2024-01-16 12:13:56 -0500

    Finish 05_sumAll exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+  if (typeof n1 !== "number" || typeof n2 !== "number" || n1 < 0 || n2 < 0) {
+    return "ERROR";
+  }
+  const diff = Math.abs(n2 - n1) + 1;
+  let sum = 0;
 
+  for (let i = 1; i <= diff; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
