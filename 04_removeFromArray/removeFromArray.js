2dc64f75add58b65b9dcc42997998ce2f8a8710f
ale-morillas <alecorm.edx@gmail.com>
2024-01-16 11:57:06 -0500

    Finish 04_removeFromArray exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  const arrCopy = [];
 
+  arr.forEach((element) => {
+    if (!args.includes(element)) {
+      arrCopy.push(element);
+    }
+  });
+
+  return arrCopy;
 };
 
 // Do not edit below this line
