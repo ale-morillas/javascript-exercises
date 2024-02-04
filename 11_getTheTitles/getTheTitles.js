d1f76efc9053fd952a7b569c328f502809ba045f
ale-morillas <alecorm.edx@gmail.com>
2024-02-04 18:00:02 -0500

    Add 11-getTheTitles exercise done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -2,18 +2,5 @@ const getTheTitles = function (array) {
   let newArray = array.map((item) => item.title);
   return newArray;
 };
-
-const books = [
-  {
-    title: "Book",
-    author: "Name",
-  },
-  {
-    title: "Book2",
-    author: "Name2",
-  },
-];
-
-console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
